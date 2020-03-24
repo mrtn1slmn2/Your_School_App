726c32c6afa09cac5a3330154c348c4cf0f258b5
mrtn1slmn2 <ag447763.martin@gmail.com>
2020-03-24 13:03:46 -0400

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/yourschoolapp/Details.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.yourschoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
